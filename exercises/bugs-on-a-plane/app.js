7b0ab517067a4e4b564d44f4e9f3362c13d619ed
Justin <justinjwillmore@gmail.com>
2018-07-31 22:51:50 -0600

    Finished bugs-on-a-plane assignment to pass off

--- exercises/bugs-on-a-plane/app.js
+++ exercises/bugs-on-a-plane/app.js
@@ -11,47 +11,25 @@ function formAlert() {
     var age = form.elements.namedItem("age").value;
     var gender = form.elements.namedItem("gender").value;
     var location = form.elements.namedItem("travel-location").value;
-    // var gQuery = document.querySelector("input[name='diet']").value;
-    // var pQuery = document.querySelector("input[name='diet']").value;
     
     function getDiet(){
         var diet = [];
         var query = document.querySelectorAll("input[name='diet']:checked")
         var vegan = document.getElementById("vegan")
         var gluten = document.getElementById("gluten")
+        var paleo = document.getElementById("paleo")
 
         for(var i = 0; i < query.length; i++){
             if (form.elements.namedItem('vegan').checked) {
                 diet.push(vegan.value);
-            } else if (form.elements.namedItem('gluten').checked) {
+            } if (form.elements.namedItem('gluten').checked) {
                 diet.push(gluten.value);
+            } if (form.elements.namedItem('paleo').checked) {
+                diet.push(paleo.value);
             }
-            // } else if (form.elements.namedItem('paleo').checked) {
-            //     diet.push(document.getElementById('paleo').value);
-            // }
-            return diet[i];
+            return diet;
             } 
         }
-    // console.log(getDiet())
     
     alert("First Name: " + firstName + "\nLast Name: " + lastName + "\nAge: " + age + "\nGender: " + gender + "\nTravel Location: " + location + "\nDiet: " + getDiet() + "\nAwesome, now if you die, it won't be an accident..");
 }
-
-
-
-// Original getDiet function
-// function getDiet(){
-//     var diet = [];
-//     var vQuery = document.querySelectorAll("input[name='diet']:checked")
-//     for(var i = 0; i < vQuery.length; i++){
-//         if (form.elements.namedItem('vegan').checked) {
-//             diet.push(document.getElementById("vegan").value);
-//         // } else if (form.elements.namedItem('gluten').checked) {
-//         //     diet.push(document.getElementById('gluten').value);
-//         // } else if (form.elements.namedItem('paleo').checked) {
-//         //     diet.push(document.getElementById('paleo').value);
-//         // }
-//         return diet;
-//         } 
-//     }
-// }
