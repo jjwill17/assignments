71aeae5337dd8ebc1c58597053f0f2e63ee60625
Justin <justinjwillmore@gmail.com>
2018-09-10 11:55:16 -0600

    added placeholder image in case api image key does not have one

--- projects/api-project/src/components/SearchResults.js
+++ projects/api-project/src/components/SearchResults.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import '../app.css'
+import NoImg from './no-image.jpg'
 
 const SearchResults = props => {
 
@@ -7,7 +8,7 @@ const SearchResults = props => {
         <div>
             <h2>{props.Title}</h2>
             <h2>{props.Year}</h2>
-            <img src={props.Poster} className='imgStyle'/>
+            <img src={props.Poster !== 'N/A' ? props.Poster : NoImg} className='searchImgStyle'/>
         </div>
     )
 }
